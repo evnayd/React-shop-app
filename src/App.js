34a6c7323723c454fc6f63aa0f660f1101a45b28
Evgeniya <evgeniya@ultimatemovers.net>
2022-09-09 15:26:11 +0200

    added slider component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import React from 'react';
 import { Cart, Filter, Header, Shop, Slider, Sort } from './components';
 
+
 function App() {
   return (
     <div className="App app-container">
       <Header/>
+      <Slider/>  
       <Cart/>
       <Filter/>    
       <Shop/>
-      <Slider/>
+  
       <Sort/>
   </div>
   );
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import {  Pagination, Navigation } from "swiper";
+
 const Slider = ( ) => {
-    return <h1>i'm Slider!</h1>
+    return (
+    <Swiper
+        spaceBetween={0}
+        centeredSlides={true}
+        pagination={{
+          clickable: true}}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper h-80"
+        loop={true}
+        style={{
+          "--swiper-navigation-color": "#fff",
+          "--swiper-pagination-color": "#fff",
+        }}
+      >
+        <SwiperSlide>
+        <img src="https://swiperjs.com/demos/images/nature-1.jpg"
+        alt="colors" />
+          </SwiperSlide>
+        <SwiperSlide>
+        <img src="https://swiperjs.com/demos/images/nature-2.jpg"
+         alt="colors" />
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src="https://swiperjs.com/demos/images/nature-3.jpg"
+         alt="colors" />
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src="https://swiperjs.com/demos/images/nature-4.jpg"
+         alt="colors" />
+        </SwiperSlide>
+      </Swiper>
+    )
 }
 
 export default Slider;
