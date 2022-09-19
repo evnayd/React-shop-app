e1f5969a4331acf0c4c848cf48ea7cb156e2ca46
Evgeniya <evgeniya@ultimatemovers.net>
2022-09-19 13:20:38 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import { Cart, Header, Shop, Slider } from './components';
 export const AppContext = React.createContext();
 
 function App() {
+  const [quantity, setQuantity] = useState(() => {
+    // getting stored value
+    const saved = localStorage.getItem("cart");
+    const itemsInCart = JSON.parse(saved);
+    console.log(itemsInCart, 'items in cart')
+    return itemsInCart.length
+  });
 
   const [ CartOpen, setCartOpen] = useState('false')
-  const [ ItemsCount, setItemsCount] = useState('0')
+  //const [ ItemsCount, setItemsCount] = useState('0')
 
   return (
 
-    <AppContext.Provider value={{ CartOpen, setCartOpen, ItemsCount, setItemsCount }}  
+    <AppContext.Provider value={{ CartOpen, setCartOpen, quantity }}  
     className="App relative">
       <Header/>
       <Slider/> 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,8 @@ import { AppContext } from "../App";
 
 const Header = ( ) => {
     const [isOpen, setOpen] = useState(false);
-    const { setCartOpen,  ItemsCount, setItemsCount  } = useContext(AppContext);
+    const { setCartOpen} = useContext(AppContext);
+    const {quantity } = useContext(AppContext);
 
     return  (
     <div className="app-container flex py-3 md:pt-6  md:pb-2 justify-between items-end md:items-baseline border-b relative z-20">
@@ -31,7 +32,7 @@ const Header = ( ) => {
       <VscHeart  className='w-5 h-5  mr-2'/>
       </div>
       <div onClick={() => setCartOpen('true')}
-       className='cursor-pointer mb-1 h-6 w-6 bg-teal-600 rounded-full flex items-center justify-center'>{ItemsCount}</div>
+       className='cursor-pointer mb-1 h-6 w-6 bg-teal-600 rounded-full flex items-center justify-center'>{quantity}</div>
         </div>    
         </div>
       </div>
